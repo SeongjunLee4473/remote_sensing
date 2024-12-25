aca527094ff7638b5a1765fdcb1138e97ab5edc3
Seongjun Lee <seongjunlee4473@gmail.com>
2024-12-25 23:48:46 +0900

    Create __init__.py

---
+++ water_quality/__init__.py
@@ -0,0 +1,8 @@
+'''
+This file initializes the package and facilitates the import of modules.
+
+It allows the directory to be treated as a package, enabling the import of 
+modules and sub-packages. This file can also be used to define package-level 
+variables, import key components, or execute initialization code necessary for 
+the package's functionality.
+'''
