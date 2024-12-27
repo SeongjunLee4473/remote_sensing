4d6d5d88963c540d7414eed78b784fb1f662cb48
Seongjun Lee <seongjunlee4473@gmail.com>
2024-12-27 16:50:10 +0900

    Classify raster codes from obs.py

--- water_quality/obs.py
+++ water_quality/obs.py
@@ -3,8 +3,6 @@
 import numpy as np
 import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
-import rasterio
 import folium
 #---------------------------------------------------------------------------------------------------#
 # Function to load and prepare data
@@ -42,34 +40,6 @@ def points_to_gdf(csv_file_path):
 # Example usage:
 csv_file_path = 'path/to/obs_data.csv'
 obs_geometry_gdf = points_to_gdf(csv_file_path)
-'''
-#---------------------------------------------------------------------------------------------------#
-# Read raster data
-def read_raster(raster_file_path):
-    '''
-    This function loads raster data from a specified file and returns the raster data
-    and its affine transform.
-
-    Last modified: 2024-12-27 / Add raster_crs (To check the coordinate reference system)
-    Created on: 2024-12-27
-    Created by: Seongjun Lee
-    '''
-    # Load Pekel Data
-    dataset = rasterio.open(raster_file_path)
-
-    # Get the affine transform and data from the dataset
-    raster_transform = dataset.transform
-    raster_data = dataset.read(1)
-    
-    # Get the CRS from the dataset
-    raster_crs = dataset.crs
-
-    return raster_data, raster_transform, raster_crs
-
-'''
-# Example usage:
-raster_file_path = 'path/to/raster.tif'
-raster_data, raster_transform = read_raster(raster_file_path)
 '''
 #---------------------------------------------------------------------------------------------------#
 # Function to extract pixel values around points
@@ -201,110 +171,5 @@ def plot_points(csv_file_path, ptNo):
 csv_file_path = '/home/seongjun/water_quality/obs_data/obs_coordinate_2024.csv'
 point_code = '3008B40'  # Change to the ptNo you want to plot (If you type 4 digits, all points with the same 4 digits will be plotted)
 plot_points(csv_file_path, point_code)
-'''
-#---------------------------------------------------------------------------------------------------#
-def plot_raster(raster_file,
-                transform=None,
-                bounds=None, 
-                title=None, 
-                label=None, 
-                cmap=None, 
-                vmin=None, 
-                vmax=None, 
-                tick_interval=None,
-                grid=True):
-    '''
-    This function plots raster data.
-
-    Last modified: None / None
-    Created on: 2024-12-27
-    Created by: Seongjun Lee
-    '''
-    # Read raster data
-    if isinstance(raster_file, np.ndarray):  # Check if raster_file is a numpy array
-        raster_data = raster_file
-        if transform is None:
-            raise ValueError("Transform must be provided when raster_data is a numpy array.")
-    elif transform is None:  # Use raster file path
-        raster_data, transform, raster_crs = read_raster(raster_file)
-        # Check if the coordinate reference system is EPSG:4326
-        if raster_crs.to_string() != 'EPSG:4326':
-            raise ValueError(f"This code is based on the WGS 84 coordinate system, "
-                             f"please reproject to EPSG:4326 and try again. \n"
-                             f"Current CRS: {raster_crs.to_string()}.")
-    elif transform is not None and raster_file.endswith('.tif'):
-        raise ValueError("You have provided both a raster file path and transform. \n"
-                         "Please input either just the raster file path without transform or "
-                         "provide the already loaded raster data along with its transform.")
-    else:
-        raster_data, transform = raster_file, transform
-
-    # Plot
-    plt.figure(figsize=(12, 8))
-
-    # Set default values if not provided
-    cmap = cmap if cmap is not None else 'viridis'
-    vmin = vmin if vmin is not None else np.nanmin(raster_data)
-    vmax = vmax if vmax is not None else np.nanmax(raster_data)
-
-    # Display the raster data
-    plt.imshow(raster_data, extent=(
-        transform[2], transform[2] + transform[0] * raster_data.shape[1],
-        transform[5] + transform[4] * raster_data.shape[0], transform[5]),
-        cmap=cmap, vmin=vmin, vmax=vmax, interpolation='none')
-
-    # Set the limits based on the provided extent
-    if bounds is not None:
-        plt.xlim(bounds[0], bounds[1])
-        plt.ylim(bounds[2], bounds[3]) 
-
-    # Title
-    plt.title(title if title is not None else 'Raster Data Visualization', fontsize=20, fontweight='bold')
-
-    # Colorbar
-    cbar = plt.colorbar(label=label if label is not None else 'Value', orientation='vertical')
-    cbar.ax.set_ylabel(label if label is not None else 'Value', fontsize=15)
-    cbar.ax.tick_params(labelsize=15)
-
-    # X/Y Ticks
-    if tick_interval is not None:
-        x_ticks = np.arange(bounds[0], bounds[1] + tick_interval, tick_interval)
-        y_ticks = np.arange(bounds[2], bounds[3] + tick_interval, tick_interval)
-        plt.xticks(x_ticks, fontsize=13, rotation=45)
-        plt.yticks(y_ticks, fontsize=13)
-    else:
-        plt.xticks(fontsize=13, rotation=45)
-        plt.yticks(fontsize=13)
-
-    # Grid
-    if grid:
-        plt.grid(True, linestyle='--', color='gray', alpha=0.5)
-
-    # Show the plot
-    plt.show()
-
-'''
-# Example usage
-1. If you want to directly load raster file
-raster_file_path = '/home/seongjun/water_quality/obs_data/pekel_data/pekel_2024.tif'
-plot_raster(raster_file=raster_file_path,
-            bounds=[127.0, 127.5, 35.0, 35.5],
-            title='Pekel Data',
-            label='Percentage of Water Occurence (%)',
-            cmap='Blues',
-            vmin=0,
-            vmax=100,
-            tick_interval=1)
-
-2. If you already have raster array and transform array
-plot_raster(raster_file=pekel_data,
-            transform=pekel_transform,
-            bounds=[127.0, 127.5, 35.0, 35.5],
-            title='Pekel Data',
-            label='Percentage of Water Occurence (%)',
-            cmap='Blues',
-            vmin=0,
-            vmax=100,
-            tick_interval=1)
 '''
 #---------------------------------------------------------------------------------------------------#
